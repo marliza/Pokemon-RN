554cddd198f3fed96f596e8a5a3225b6a2b319e2
marliza <marliza.viegas@gmail.com>
2021-05-02 17:31:08 +0100

    refactor code

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,7 @@
 export const BASE_URL = 'https://pokeapi.co/api';
 export const ENDPOINT = '/v2/pokemon';
 
+export const APP_TITLE = 'Pokemon';
+export const SEARCHBAR_PLACEHOLDER = 'Search Pokemon';
+
 export const PLACEHOLDER_IMAGE = require('./assets/pokeball.png');
